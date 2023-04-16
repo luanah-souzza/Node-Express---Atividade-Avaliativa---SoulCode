2d0e5d2b1ec1b800b72a613c22e7d8e5ffc0c950
luanah-souzza <luanah.souzza@hotmail.com>
2023-04-16 20:43:00 -0300

    Adicionando configuração do MORGAN.log

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const usuarios= require("./routes/usuarios");
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 
 
 const app = express();
 app.use(express.json());
 
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'morgan.log'), { flags: 'a' });
+
+app.use(morgan('combined', { stream: accessLogStream }));
+
+// configura o método de recebimento das requisições para JSON
+app.use(express.json());
+
+
+
 app.use("/usuarios", usuarios);
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000/");
+  });
